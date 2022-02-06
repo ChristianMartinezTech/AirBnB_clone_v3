95ab7afb7bfd72ad062497a22baa0f67ef0095dd
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-06 13:00:54 -0500

    Fixing flask error

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,11 +10,11 @@ from flask_cors import CORS
 # Instanciating Flask in app
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exeption):
+def teardown_appcontext(self):
     """Removes the sqlalchemy session"""
     storage.close()
 
