c2efe6ee83cf05b9467e8e251d0276333ddfeeb2
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-06 14:06:58 -0500

    Fixing output

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-
 @app.teardown_appcontext
 def teardown_appcontext(self):
     """Removes the sqlalchemy session"""
@@ -19,12 +18,7 @@ def teardown_appcontext(self):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    if host is None:
-        host = '0.0.0.0'
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
 
-    port = getenv('HBNB_API_PORT')
-    if port is None:
-        port = 5000
-
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=int(port), threaded=True, debug=True)
