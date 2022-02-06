2429c413b294c0b54b6cdba348edf24d8c9a7571
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-06 17:59:36 -0500

    Coded 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,14 @@
 """route /status on the object app_views that returns a JSON:
 "status": "OK"""
 from api.v1.views import app_views
-from flask import jsonify, Flask
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -15,9 +21,9 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def counter():
     """endpoint that retrieves the number of each objects by type"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("cities"),
-                    "places": storage.count("places"),
-                    "reviews": storage.count("reviews"),
-                    "states": storage.count("states"),
-                    "users": storage.count("users")})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
