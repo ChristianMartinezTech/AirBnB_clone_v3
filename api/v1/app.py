28e41c20f919f2107ca10851c71cc07713957c97
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-06 13:29:38 -0500

    Fixing MySQL

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,11 @@ from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
 
 # Instanciating Flask in app
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 
 @app.teardown_appcontext
